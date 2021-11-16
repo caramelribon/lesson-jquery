2770b0324f3cf1d11900fd751bae9ce69ca5ee1f
wakana.tatsuta <caramelribon137@gmail.com>
2021-11-16 10:02:12 +0000

    first commit

---
+++ acodionmenu/main.js
@@ -0,0 +1,20 @@
+// アコーディオンのタイトルがクリックされたら
+$('.accordion-title a').on('click', (e) => {
+  // hrefにページ遷移しない
+  e.preventDefault();
+
+  // 同じsection内の.accordion-contentを選択
+  const content = $(e.target)
+    .closest('section')
+    .find('.accordion-content');
+
+  // .accordion-contentが非表示の場合は
+  if (!content.is(':visible')) {
+    // 表示中のコンテンツを閉じる
+    $('.accordion-content:visible').slideUp();
+
+    // クリックされたコンテンツを表示
+    content.slideDown();
+  }
+  //$(セレクタ).is(":visible") は、セレクタに該当する要素が表示されていれば true、非表示なら false になります。
+});
